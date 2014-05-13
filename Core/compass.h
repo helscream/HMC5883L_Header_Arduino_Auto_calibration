c43522a3382896115ccb6c7e23b8df86237bade2
Omer Ikram ul Haq <omer53@live.com>
2014-05-13 10:19:53 +0200

    Addition of Core files and Read Me

---
+++ Core/compass.h
@@ -0,0 +1,21 @@
+#ifndef compass.h
+  #define compass.h
+  
+  #include "compass.h"
+  
+  extern float bearing;
+  extern float compass_x_scalled;
+  extern float compass_y_scalled;
+  extern float compass_z_scalled;
+  
+  extern float compass_x_offset, compass_y_offset, compass_z_offset;
+  extern float compass_x_gainError,compass_y_gainError,compass_z_gainError;
+  
+  void compass_read_XYZdata();
+  void compass_offset_calibration(int select);
+  void compass_init(int gain);
+  void compass_scalled_reading();
+  void compass_heading();
+  
+  
+#endif
